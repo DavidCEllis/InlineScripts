27cf9d5e4d12daed2cffaf86303e5dc411cacc67
David C Ellis <ducksual@gmail.com>
2025-07-22 15:12:39 +0100

    Remove Studio One workaround, loosely pin dependencies, documentation

--- wip_scripts/merge_midi_expression.py
+++ wip_scripts/merge_midi_expression.py
@@ -1,23 +1,38 @@
 # /// script
 # requires-python = "~=3.12.0"
 # dependencies = [
-#     "ducktools-classbuilder",
-#     "mido[ports-rtmidi]",
+#     "ducktools-classbuilder~=0.10.0",
+#     "mido[ports-rtmidi]~=1.3.3",
 # ]
 # ///
+"""
+This script is intended to create a virtual MIDI device that combines the 
+aftertouch of an MPE instrument such as the Seaboard that has no expression
+pedal input, with the expression input from another MIDI device.
+
+The expression pedal is used to set a minimum value for aftertouch input.
+Aftertouch from the MPE device is then rescaled to go from this new minimum
+to the maximum of 127.
+
+This will need modification to work on Windows as mido is unable to create a
+virtual port on that platform.
+See: https://mido.readthedocs.io/en/latest/backends/rtmidi.html#virtual-ports
+"""
 import sys
+import argparse
 from math import ceil
 
 import mido
 
 from ducktools.classbuilder.prefab import Prefab
 
-SEABOARD_NAME = "Seaboard BLOCK M"
-EXPRESSION_NAME = "MIDI4x4 Midi In 3"  # Kurzweil with expression pedal
+# When run directly as a script these are the device names that will be used
 VIRTUAL_DEVICE_NAME = "Virtual Seaboard"
+MPE_DEVICE_NAME = "Seaboard BLOCK M"
+EXPRESSION_DEVICE_NAME = "MIDI4x4 Midi In 3"  # Name of the device with the expression pedal
 
 EXPRESSION_CC = 11
-BRIGHTNESS_CC = 74
+MAX_EXPRESSION = 64
 
 
 def check_inputs(midi_inputs: list[str]) -> bool:
@@ -37,17 +52,27 @@ class MidiModifier(Prefab):
     and uses it to "compress" the values of pressure from the main_source.
     This is done by raising the floor value of the pressure messages and 
     adjusting the in-between values accordingly.
+
+    :param main_source: The name of your main MPE device
+    :param expression_source: The name of the MIDI device to take Expression input from
+    :param virtual_device: The name of the new virtual device to create
+    :param max_expression: The value that the expression pedal at maximum level sets as 
+                           the minimum aftertouch value
+    :param filter_sysex: Remove sysex messages from the midi output of the virtual device
     """
+    virtual_device: str
     main_source: str
     expression_source: str
-    virtual_device: str = VIRTUAL_DEVICE_NAME
 
-    max_expression: int = 64
-
-    filter_sysex: bool = True  # The seaboard sends a bunch of sysex I want to filter
+    expression_cc: int
+    max_expression: int
+    filter_sysex: bool = True
 
     @staticmethod
     def __prefab_pre_init__(main_source, expression_source):
+        """
+        Check to make sure both named devices are currently available
+        """
         known_inputs = mido.get_input_names()  # type: ignore
 
         for device in (main_source, expression_source):
@@ -55,7 +80,10 @@ class MidiModifier(Prefab):
                 raise RuntimeError(f"{device!r} input is not enabled")
     
     def print_messages(self) -> None:
-        # A simple print function to show the outputs
+        """
+        Simple print function to show the source aftertouch and expression values.
+        """
+
         with mido.open_input(self.main_source) as main, mido.open_input(self.expression_source) as expr:  # type: ignore
             try:
                 for pair in mido.ports.multi_receive([main, expr], yield_ports=True):
@@ -78,9 +106,11 @@ class MidiModifier(Prefab):
         """
         last_expression = 0
         scaling = self.max_expression / 128  # 128 so 64 is exactly half
-        last_pressures = {} # Last pressure messages for each channel before scaling
-        expression_cc = EXPRESSION_CC
-        brightness_cc = BRIGHTNESS_CC
+
+        # Need to keep track of the last pressure values for each channel before scaling
+        # When the expression pedal is moved it will send new re-scaled aftertouch messages
+        # for every channel.
+        last_pressures = {}
 
         print(f"Starting virtual device with scaling: {scaling} - Pressure Mode")
         print("Press ctrl + c to close")
@@ -89,97 +119,63 @@ class MidiModifier(Prefab):
                 try:
                     for pair in mido.ports.multi_receive([main, expr], yield_ports=True):
                         device, message = pair
+
                         if self.filter_sysex and message.type == "sysex":
                             continue
 
                         if device.name == self.expression_source:
-                            if message.is_cc(expression_cc):
+                            # If expression is changed, send out updates for *all* aftertouch values with new scaling
+                            if message.is_cc(self.expression_cc):
                                 last_expression = message.value
                                 for pressure in last_pressures.values():
                                     new_value = ceil(pressure.value * (1 - (last_expression/127 * scaling)) + last_expression * scaling)
                                     new_message = pressure.copy(value=new_value)
                                     midi_out.send(new_message)
                                 
-                        else:  # Must be main_source, no need to check
+                        else:  # Must be main_source, no need to check device name
                             if message.type == "aftertouch":
+                                # Store unscaled aftertouch values
+                                last_pressures[message.channel] = message
+
                                 # Aftertouch scaling calculation
                                 new_value = ceil(message.value * (1 - (last_expression/127 * scaling)) + last_expression * scaling)
-                                last_pressures[message.channel] = message
                                 new_message = message.copy(value=new_value)
                                 midi_out.send(new_message)
                             else:
+                                # Non-aftertouch messages sent unaltered
                                 midi_out.send(message)
-                                if message.type == "note_on":  # Hack for Studio One 5
-                                    # Send a brightness reset for Studio One which sets it to 0 on note on for some reason?
-                                    # So we send a default value *after* the initial note to keep it at the midpoint
-                                    brightness = mido.Message("control_change", channel=message.channel, control=brightness_cc, value=64)
-                                    midi_out.send(brightness)
 
                 except KeyboardInterrupt:
                     pass
         
         print("Closing")
 
-    def start_virtual_expression(self) -> None:
-        """
-        Start the virtual device, relaying main_source with expression values
-        """
-        # localise variables
-        expression_cc = EXPRESSION_CC
-        brightness_cc = BRIGHTNESS_CC
 
-        last_expression = mido.Message("control_change", control=expression_cc, channel=0, value=0)  # type: ignore
-
-        scaling = self.max_expression / 128  # 128 so 64 is exactly half
-        last_pressure = 0 # Last pressure message before scaling
-
-        print(f"Starting virtual device with scaling: {scaling} - Expression Mode")
-        print("Press ctrl + c to close")
-        with mido.open_output(self.virtual_device, virtual=True) as midi_out:  # type: ignore
-            with mido.open_input(self.main_source) as main, mido.open_input(self.expression_source) as expr:  # type: ignore
-                try:
-                    for pair in mido.ports.multi_receive([main, expr], yield_ports=True):
-                        device, message = pair  # type: ignore
-                        
-                        if self.filter_sysex and message.type == "sysex":
-                            continue
-
-                        if device.name == self.expression_source:
-                            if message.is_cc(expression_cc):
-                                last_expression = message
-                                
-                                new_value = ceil(last_pressure * (1 - (last_expression.value/127 * scaling)) + last_expression.value * scaling)  # type: ignore
-                                new_message = message.copy(value=new_value)
-                                midi_out.send(new_message)
-                                
-                        else:  # Must be main_source, no need to check
-                            if message.type == "aftertouch":
-                                last_pressure = message.value
-
-                                # Aftertouch scaling calculation
-                                new_value = ceil(message.value * (1 - (last_expression.value/127 * scaling)) + last_expression.value * scaling)  # type: ignore
-                                
-                                # Send it out as a new expression value
-                                new_message = last_expression.copy(value=new_value)
-                                midi_out.send(new_message)
-                            else:
-                                midi_out.send(message)
-                                if message.type == "note_on":  # Hack for Studio One 5
-                                    # Send a brightness reset for Studio One which sets it to 0 on note on for some reason?
-                                    # So we send a default value *after* the initial note to keep it at the midpoint
-                                    brightness = mido.Message("control_change", channel=message.channel, control=brightness_cc, value=64)
-                                    midi_out.send(brightness)
-
-                except KeyboardInterrupt:
-                    pass
-        
-        print("Closing")
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--list-devices", action="store_true")
+    return parser
 
 
 def main() -> int:
-    
-    modifier = MidiModifier(SEABOARD_NAME, EXPRESSION_NAME)
-    modifier.start_virtual_expression()
+    if len(sys.argv) > 1:
+        parser = get_parser()
+        args = parser.parse_args()
+        if args.list_devices:
+            print("Valid MIDI input device names:")
+            for item in mido.get_input_names():
+                print(f"\t{item!r}")
+            return 0
+
+    modifier = MidiModifier(
+        virtual_device=VIRTUAL_DEVICE_NAME,
+        main_source=MPE_DEVICE_NAME, 
+        expression_source=EXPRESSION_DEVICE_NAME,
+        expression_cc=EXPRESSION_CC,
+        max_expression=MAX_EXPRESSION,
+        filter_sysex=True,
+    )
+    modifier.start_virtual_aftertouch()
 
     return 0
 
